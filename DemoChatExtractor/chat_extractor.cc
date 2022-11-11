1a074b1f182c25abda270ff220366b757296d5ed
Sil <sildak@gmail.com>
2022-11-11 04:24:34 +0100

    added chat extractor code

---
+++ DemoChatExtractor/chat_extractor.cc
@@ -0,0 +1,181 @@
+#include "..\DemoManipulator\demo.h"
+#include <iostream>
+#include <fstream>
+
+using namespace DemoJKA;
+using namespace std;
+
+ofstream outputFile;
+
+enum {
+	MOD_RAW_TEXT,
+	MOD_COLORED_TEXT,
+	MOD_HTML
+};
+
+static const char* htmlcolors[] = 
+{
+	"\"#000000\"", //0
+	"\"#FF0000\"", //1
+	"\"#00FF00\"", //2
+	"\"#FFFF00\"", //3
+	"\"#0000FF\"", //4
+	"\"#00FFFF\"", //5
+	"\"#FF00FF\"", //6
+	"\"#FFFFFF\"", //7
+};
+
+string sanitize(const string& command, int mod){
+	//dest <= souce always (we only remove from source, never add)
+	static char buffer[2048];
+	int sourceId = 0;
+    char* dest = buffer;
+
+	for(;command[sourceId]!=0;++sourceId)
+	{
+		if ((command[sourceId] == '^' && (command[sourceId+1]>='0') && (command[sourceId+1]<='7')) &&
+			(mod == MOD_HTML || mod == MOD_RAW_TEXT)) {
+
+			if (mod == MOD_HTML){
+				strcpy_s(dest,40,"</font><font color=");
+				dest += strlen("</font><font color=");
+
+				strcpy_s(dest,20,htmlcolors[command[sourceId+1]-'0']);
+				dest += strlen(htmlcolors[command[sourceId+1]-'0']);
+
+				*dest = '>';
+				++dest;
+			}
+
+			//need to increment here, two increments must be done
+			++sourceId;
+
+		} else if (command[sourceId] == 25 || command[sourceId] == '"'){
+			continue;
+		} else {
+			*dest = command[sourceId];
+			++dest;
+		}
+	}
+	*dest = 0;
+
+	return string(buffer);
+}
+
+void logCommand(const string& command, int mod){
+	const char* str = command.c_str();
+	string chatLine;
+
+	if (!strncmp(str,"chat",4)){
+		chatLine = command.substr(5);
+	} else if (!strncmp(str,"tchat",5)){
+		chatLine = command.substr(6);
+	} else {
+		return;
+	}
+
+	chatLine = sanitize(chatLine, mod);
+
+	if (mod == MOD_HTML){
+		outputFile << "<font color=#FFFFFF>";
+	}
+
+	outputFile << chatLine;
+
+	if (mod == MOD_HTML){
+		outputFile << "</font><br>";
+	}
+
+	outputFile << endl;
+}
+
+
+
+int main(int argc, char** argv){
+
+	//-html
+	//-text
+	//-colored
+	int mod = MOD_RAW_TEXT;
+
+	if (argc < 3){
+		cout << "Need more arguments." << endl;
+
+		cout << "Usage: DemoExtractor [input] [output] (parameter)" << endl;
+		cout << "   input - input demo file, full name (with .dm_26 extension)" << endl;
+		cout << "   output - output text/html file, full name (with .htm, .txt or w/e extension)" << endl;
+		cout << "   parameter - can be one of following:" << endl;
+		cout << "               -text - normal text will be generated (default)" << endl;
+		cout << "               -html - special html file with colors will be generated" << endl;
+		cout << "               -code - normal text WITH color codes will be generated" << endl;
+		return 1;
+	}
+
+	if (argc >= 4){ //parameters
+		if (!strncmp(argv[3],"-html",5)){
+			mod = MOD_HTML;
+		} else if (!strncmp(argv[3],"-text",5)){
+			mod = MOD_RAW_TEXT;
+		} else if (!strncmp(argv[3],"-code",5)){
+			mod = MOD_COLORED_TEXT;
+		}
+	}
+
+	Demo  inputDemo;
+	outputFile.open(argv[2]);
+
+	if (!inputDemo.open(argv[1])){
+		cout << "Input demo could not be opened." << endl;
+		return 1;
+	}
+
+	if (outputFile.fail()){
+		cout << "Output file could not be opened." << endl;
+		return 1;
+	}
+
+	int count = inputDemo.getMessageCount();
+	int lastSeqNumber = 0;
+
+	if (mod == MOD_HTML){
+		outputFile << "<html>\n<head></head>\n<body bgcolor=gray>\n<b>";
+	}
+
+	for(int i=0; i<count; ++i){
+		Message* message = inputDemo.getMessage(i);
+
+		if (!message)
+			continue;
+
+		for(int j=0; j< message->getInstructionsCount(); ++j){
+			Instruction* instruction = message->getInstruction(j);
+			ServerCommand* serverCommand = instruction->getServerCommand();
+			if (serverCommand){
+				int seqNumber = serverCommand->getSequenceNumber();
+
+				if (seqNumber <= lastSeqNumber){
+					continue;
+				}
+
+				lastSeqNumber = seqNumber;
+
+				logCommand(serverCommand->getCommand(), mod);
+
+
+			}
+
+		}
+
+		inputDemo.unloadMessage(i);
+
+	}
+
+	if (mod == MOD_HTML){
+		outputFile << "\n</b>\n</body>\n</html>";
+	}
+
+	inputDemo.close();
+	outputFile.close();
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,27 +1,37 @@
-This is code of the Demo Cutter tool created long time ago for the Jedi Academy demo files. In 2022 I've stumbled upon it on my drive and decided to put it on Github before it gets lost. However this project is no longer developed or maintained. The code is almost without change as it was when last the update was done in 2013, only changes done were to make it compilable with Qt 6.X.X and some quick integration with conan & cmake.
+This repository contains C/C++ utilities I made long time ago for parsing the Jedi Academy demo files (*.dm_26). In 2022 I've stumbled upon it on my drive and I've decided to put it on Github before it gets lost. This project is no longer developed or maintained. The code is almost exactly as it was when the last the update was done in 2013, only recent changes have been done to integrate it with latest Qt 6.X.X and also with conan and cmake.
 
-To properly download and build dependencies, conan package manager is used.
+To properly download and build the dependencies (basically just Qt and its dependencies), the conan package manager is used.
 
-Make solution for release x64 version in VS 2022:
+To make release x64 solution for Visual Studio 2022:
 
     conan install . --install-folder build --build=missing
     cmake . -G "Visual Studio 17 2022" -B build
 
-Downloading and building all the dependencies wiill take some time. Solution file will be generated in build/JkaDemoTools.sln. Selecting the desired project and changing the build from Debug to Release compiles fine in VS2022.
+Downloading and building all the dependencies will take some time. Solution file will be generated in build/JkaDemoTools.sln. Selecting the desired project and changing the build from Debug to Release compiles fine in VS2022.
 
-Some things that were not done during conversion:
+Some things that have not been done during the conversion:
  - the icon is not used in the Qt 6, should be simple update if needed
+ - the code is in a bad shape, it's one of my first C++ works, so it even has a commented code!
 
 Following is a summary of included components based on my recolection. Some of these tools were released on jkhub.org back in the days.
 
 # Demo Manipulator
-Common source files for tools used to parse the demo files.
+Common source files for tools for parsing the demo files.
 
 # Demo Cutter
 GUI Qt base application for cliping the demo file into shorter and smaller demo file, based on selected time range.
 
+Slightly older version of the tool was released [here](https://jkhub.org/files/file/1342-demo-cutter/).
+
 # Demo Smoother
 Command-line tool that optimizes selected demo file for size as well as makes the players movements smoother (remove the "laggy" movements caused by network latency).
+Example use:
+
+    DemoSmoother 1431_ctf4.dm_26 1431_ctf4_smoothered.dm_26
 
 
+# Demo Chat Extractor
+Command-line for extracting chat from the demo into either text file or html file (with colors formated). Example use:
+
+    DemoChatExtractor 1952_ctf_nelvaan.dm_26 chat.html -html
 
